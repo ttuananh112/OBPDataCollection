de520f09aac4c36c4c0dea481263b60a0c942a98
Tran Tuan Anh <anhtt163@vingroup.net>
2021-11-27 09:55:50 +0700

    Refactor: shorten function to get data

--- libs/stats/statistics.py
+++ libs/stats/statistics.py
@@ -20,6 +20,33 @@ class Statistics:
             "velocity": list(),
             "turn": list()
         }
+
+        def _get_velocity():
+            # get average velocity
+            avg_vel = list()
+            for _row in status:
+                avg_vel.append(json.loads(_row)["velocity"])
+            avg_vel = np.mean(avg_vel)
+            result["velocity"].append(avg_vel)
+
+        def _get_turning():
+            # get turning vehicle
+            # must be called after _get_velocity()
+            heading_diff = np.diff(heading + np.pi)
+            heading_sum_diff = np.sum(heading_diff)
+            heading_sum_diff = (heading_sum_diff
+                                if abs(heading_sum_diff) > 1e-2
+                                else 0)
+            if heading_sum_diff > 0:
+                result["turn"].append("left")
+            elif heading_sum_diff < 0:
+                result["turn"].append("right")
+            else:
+                if abs(result["velocity"][-1]) < 1:
+                    result["turn"].append("stay")
+                else:
+                    result["turn"].append("straight")
+
         for dynamic_file in tqdm(self._list_dynamics):
             df = pd.read_csv(dynamic_file)
             # stats by individual instance
@@ -29,25 +56,9 @@ class Statistics:
                 heading = _frame["heading"].to_numpy()
                 status = _frame["status"]
 
-                # get turning vehicle
-                heading_diff = np.diff(heading + np.pi)
-                heading_sum_diff = np.sum(heading_diff)
-                heading_sum_diff = (heading_sum_diff
-                                    if abs(heading_sum_diff) > 1e-2
-                                    else 0)
-                if heading_sum_diff > 0:
-                    result["turn"].append("left")
-                elif heading_sum_diff < 0:
-                    result["turn"].append("right")
-                else:
-                    result["turn"].append("straight")
+                _get_velocity()
+                _get_turning()
 
-                # get average velocity
-                avg_vel = list()
-                for _row in status:
-                    avg_vel.append(json.loads(_row)["velocity"])
-                avg_vel = np.mean(avg_vel)
-                result["velocity"].append(avg_vel)
         return result
 
     def plot_stats(self):
